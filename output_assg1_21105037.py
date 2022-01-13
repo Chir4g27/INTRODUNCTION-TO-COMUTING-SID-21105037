9a70a79db3e7d41d685d7b40ac3d93ea3815396f
Chir4g27 <96478040+Chir4g27@users.noreply.github.com>
2022-01-14 00:48:15 +0530

    Add files via upload

---
+++ output_assg1_21105037.py
@@ -0,0 +1,46 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: /Users/chirag/Documents/1.py ====================
+solving Q1  
+
+enter no 1 23
+enter no 2 45
+enter no 3 65
+sum of given nos is  133
+average of given nos is  44.333333333333336 
+
+solving Q2  
+
+ All values are to be entered in dollars.
+enter your gross income $700000
+enter no of dependents 4
+the tax comes out to be $ 135600.0 
+
+solving Q3  
+
+Student = [sid, name, gender, course, cgpa]
+enter your SID 21105037
+enter your name chirag
+enter your gender 'M', 'F', 'U' for male, female, unknown respectively.m
+enter your course name ece
+enter your cgpa 15
+student info  [21105037, 'chirag', 'm', 'ece', 15.0] 
+
+solving Q4 
+
+enter marks of student 1 23
+enter marks of student 2 54
+enter marks of student 3 59
+enter marks of student 4 87
+enter marks of student 5 49
+marks of 5 students are  [23, 54, 59, 87, 49]
+marks in ascending order  [23, 49, 54, 59, 87]
+marks in descending order  [87, 59, 54, 49, 23] 
+
+solving Q5 
+
+The provided list is  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'] 
+
+ The modified list  is color 1 = ['Red', 'Green', 'White', 'Pink', 'Yellow']
+The modified list is color 2 = ['Red', 'Green', 'White', 'Purple', 'Yellow']
